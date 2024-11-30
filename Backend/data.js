561f0dc84a3ea7025799519b83e97df81d3b1432
neiroc-02 <neiro.cabrera@yahoo.com>
2024-11-29 17:05:11 -0800

    Some changes, some problems to think about on how we handle global state with individual JSONs

--- Backend/data.js
+++ Backend/data.js
@@ -5,13 +5,14 @@ const { exec } = require('child_process'); //maybe to help start the docker???
 const { encrypt, decrypt } = require('eciesjs'); //
 
 const users = {}; // Stores user info by userid
-const STATE_FILE_PATH = '../state.json';
+const STATE_FILE_PATH = '../mpc/state.json';
 const ENCODING = 'utf8';
 
 function generateId() {
   return Math.random().toString(36).substr(2, 9);
 }
 
+//THIS FUNCTION IS GOOD: INITIALIZES STATE.JSON
 function initialize_state_file() {
   /*
   Initializes the state.json with the correct fields 
@@ -32,24 +33,24 @@ function initialize_state_file() {
 }
 
 //THIS FUNCTION IS GOOD: USED EVERYWHERE
-function read_state(){
-/*
-Reads and dumps the whole state.json file
-Returns the JSON.parse() of state.json
-*/
+function read_state() {
+  /*
+  Reads and dumps the whole state.json file
+  Returns the JSON.parse() of state.json
+  */
   try {
     const data = fs.readFileSync(STATE_FILE_PATH, ENCODING);
     console.log(JSON.parse(data));
     return JSON.parse(data);
   }
-  catch (err){
+  catch (err) {
     console.error('Error reading from state file');
     return null
   }
 }
 
 //THIS FUNCTION IS GOOD: MIRRORS WRITE_OUTPUT_TO_STATE()
-function write_output_to_state(new_state){
+function write_output_to_state(new_state) {
   /*
   Updates the state.json file with a new state
   Returns true on a successful write, false otherwise
@@ -60,7 +61,7 @@ function write_output_to_state(new_state){
       console.error('Uninitialized current state');
       return false;
     }
-    const update_state = {...current_state, ...new_state };
+    const update_state = { ...current_state, ...new_state };
     console.log(update_state);
     fs.writeFileSync(STATE_FILE_PATH, JSON.stringify(update_state, null, 2), ENCODING);
     console.log('Updated state file successfully');
@@ -71,37 +72,14 @@ function write_output_to_state(new_state){
   }
 }
 
-/* FIXME: REDO THIS FUNCTION TO MIRROR PY VERSION */
-function populate_input_from_state(){
-  /*
-  Populates the input from the state file
-  Returns the input mapping from state.json
-  */
-  try {
-    const current_state = read_state();
-    if (!current_state){
-      console.error("Unable to read state");
-      return {};
-    }
-    const input_mapping = current_state.inputs;
-    console.log('Inputs Mapping:', input_mapping);
-    return input_mapping;
-  }
-  catch (err) {
-    console.error('Error while populating inputs: ', err);
-    return {}
-  }
-}
-
-//THIS FUNCTION IS GOOD FOR FRONTEND CHECK
-function is_player_alive(pid){
+function is_player_alive(pid) {
   /*
   Checks if a player is alive given a pid:
   Returns true if alive, false if dead, otherwise null to designate an error state
   */
   try {
     const current_state = read_state();
-    if (!current_state){
+    if (!current_state) {
       console.error("State not loaded");
       return null;
     }
@@ -112,21 +90,20 @@ function is_player_alive(pid){
       console.log('Unknown value');
       return null;
     }
-  } catch(err) {
+  } catch (err) {
     console.error('An error occured while reading state');
     return null;
   }
 }
 
-//THIS FUNCTION IS GOOD FOR FRONTEND WRITE
-function kill_player(pid){
+function kill_player(pid) {
   /*
   Write a dead state to the state.json
   Returns true on a successful write, false otherwise
   */
   try {
     const state = read_state();
-    if (!state){
+    if (!state) {
       console.error("State not loaded");
       return false;
     }
@@ -134,26 +111,123 @@ function kill_player(pid){
     fs.writeFileSync(STATE_FILE_PATH, JSON.stringify(state, null, 2), ENCODING);
     console.log('Player is dead');
     return true;
-  } catch(err) {
+  } catch (err) {
     console.error('An error occured while reading state');
     return false;
   }
 }
 
-/* TODO: DESIGN THIS FUNCTION TO ENCRYPT DATA, WRITE TO JSON[OUTPUT] I ASSUME */
-function handle_detective_choice(){}
+function handle_detective_choice(detective_choice) {
+  /*
+    This function should handle the detective choice
+    It should encrypt the choice and write it to the state.json file
+    Returns true on a successful write, false otherwise
+  */
+  try {
+    const state = read_state();
+    if (!state) {
+      console.error("State not loaded!");
+      return false;
+    }
+    if (state.detective_private_key === "") {
+      console.error("Player is not the detective!");
+      return false;
+    }
+    const encrypted = encrypt(state.detective_private_key, detective_choice);
+    state.inputs[state.private_id] = encrypted;
+    const retval = write_output_to_state(state);
+    if (!retval) {
+      console.error("Error writing to state");
+      return false;
+    }
+  }
+  catch (err) {
+    console.error("Error handling detective choice");
+    return false;
+  }
+}
 
-/* TODO: DESIGN THIS FUNCTION TO HANDLE ANGEL/MAFIA CHOICE TO SEND IN OUTPUT */
-function handle_angel_mafia_choice(){}
+/*
+ FIXME: DESIGN THIS FUNCTION TO HANDLE ANGEL/MAFIA CHOICE TO SEND IN JSON[INPUT] 
+  Question: How do we do this given that the state files are individualized? 
+  
+*/
+function handle_angel_mafia_choice(mafia_choice, angel_choice) {
+  /*
+    This function should handle the angel/mafia choice
+    It should write these choices to JSON[INPUT] in the state.json file
+    Returns true on a successful write, false otherwise
+  */
+  try {
+    const state = read_state();
+    if (!state) {
+      console.error("State not loaded!");
+      return false;
+    }
+    //FIXME: Check if player is mafia or angel
+    state.inputs[state.private_id] = mafia_choice;
+    state.inputs[state.private_id] = angel_choice;
+    const retval = write_output_to_state(state);
+    if (!retval) {
+      console.error("Error writing to state");
+      return false;
+    }
+  } catch (err) {
+    console.error("Error handling angel/mafia choice");
+    return false;
+  }
+}
 
-/* TODO: DESIGN THIS FUNCTION TO CALL THE DOCKER IMAGE; LOW PRIORITY */
-function call_docker_image(){}
+function update_enum(){
+  /*
+  Updates the global_enum in the state.json file, incrementing by 1
+  Returns true on a successful write, false otherwise
+  */
+  try {
+    const state = read_state();
+    if (!state) {
+      console.error("State not loaded!");
+      return false;
+    }
+    state.global_enum = (parseInt(state.global_enum) + 1).toString();
+    const retval = write_output_to_state(state);
+    if (!retval) {
+      console.error("Error writing to state");
+      return false;
+    }
+  } catch (err) {
+    console.error("Error updating enum");
+    return false;
+  }
+}
 
 /* TODO: DESIGN THIS FUNCTION TO DISTRIBUTE ROLES TO PLAYERS */
-function role_distribution(){}
+function role_distribution() { }
 
 /* TODO: DESIGN THIS FUNCTION TO ASSIGN KEYS TO PLAYERS */
-function populate_keys(){}
+function populate_keys() {}
+
+function call_docker_image() {
+  /* 
+  Calls the docker image to run the MPC
+  */
+  // docker build -t mental-mafia .
+  exec('docker build -t mental-mafia .', (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(stdout);
+  });
+  // docker run --rm -it -v ./state.json:/usr/src/MP-SPDZ/state.json mental-mafia
+  exec('docker run --rm -it -v ./state.json:/usr/src/MP-SPDZ/state.json mental-mafia', (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(stdout);
+  });
+}
 
 module.exports = {
   users,
@@ -163,5 +237,7 @@ module.exports = {
   populate_input_from_state,
   write_output_to_state,
   is_player_alive,
-  kill_player
+  kill_player,
+
+  call_docker_image
 };
